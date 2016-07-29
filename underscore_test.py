786d71feb0e2f52c1b41484fda3512c55101c9d8
sheltowt <sheltowt@gmail.com>
2016-07-29 16:10:31 -0400

    adding updates

--- underscore_test.py
+++ underscore_test.py
@@ -1,5 +1,5 @@
 import unittest
-from underscore import _map, _filter, _any, _every, _contains, _reduce
+from underscore import *
 
 class TestUnderscore(unittest.TestCase):
 
@@ -12,50 +12,50 @@ class TestUnderscore(unittest.TestCase):
 
 		self.assertEqual(res, [6, 8, 10, 12, 14])
 
-	def test_filter(self):
-	    def start_with_a(word):
-	    	if word[0] == "a":
-	    		return True 
-	    	return False
+	# def test_filter(self):
+	#     def start_with_a(word):
+	#     	if word[0] == "a":
+	#     		return True 
+	#     	return False
 
-	    input_array = ["artichoke", "orangatang", "kiwi", "orange"]
-	    res = _filter(input_array, start_with_a)
+	#     input_array = ["artichoke", "orangatang", "kiwi", "orange"]
+	#     res = _filter(input_array, start_with_a)
 
-	    self.assertEqual(res, ["artichoke"])
+	#     self.assertEqual(res, ["artichoke"])
 
-	def test_any(self):
-		def greater_than_10(number):
-			if number > 10:
-				return True
-			return False
+	# def test_any(self):
+	# 	def greater_than_10(number):
+	# 		if number > 10:
+	# 			return True
+	# 		return False
 
-		input_array = [8, 5, 9, 11]
-		res = _any(input_array, greater_than_10)
-		self.assertEqual(res, True)
+	# 	input_array = [8, 5, 9, 11]
+	# 	res = _any(input_array, greater_than_10)
+	# 	self.assertEqual(res, True)
 
-	def test_every(self):
-		def greater_than_10(number):
-			if number > 10:
-				return True
-			return False
+	# def test_every(self):
+	# 	def greater_than_10(number):
+	# 		if number > 10:
+	# 			return True
+	# 		return False
 
-		input_array = [11, 15, 19, 21]
-		res = _any(input_array, greater_than_10)
-		self.assertEqual(res, True)
+	# 	input_array = [11, 15, 19, 21]
+	# 	res = _any(input_array, greater_than_10)
+	# 	self.assertEqual(res, True)
 
-	def test_contains(self):
-		input_array = [3, 4, 5, 6]
-		res = _contains(input_array, 6)
-		self.assertEqual(res, True)
+	# def test_contains(self):
+	# 	input_array = [3, 4, 5, 6]
+	# 	res = _contains(input_array, 6)
+	# 	self.assertEqual(res, True)
 
-	def test_reduce(self):
+	# def test_reduce(self):
 
-		def summer(num_1, num_2):
-			return num_1 + num_2
+	# 	def summer(num_1, num_2):
+	# 		return num_1 + num_2
 
-		input_array = [8, 5, 9, 11]
-		res = _reduce(input_array, summer, 0)
-		self.assertEqual(res, 33)
+	# 	input_array = [8, 5, 9, 11]
+	# 	res = _reduce(input_array, summer, 0)
+	# 	self.assertEqual(res, 33)
 
 
 
